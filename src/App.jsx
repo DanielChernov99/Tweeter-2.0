a16bf145ed501a484b540f12b617633e5ce007ee
DanielChernov99 <chernov.dani@gmail.com>
2026-07-07 17:29:56 +0300

    STEP 7 Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
 import HomePage from "./pages/HomePage";
 import UserPage from "./pages/UserPage";
+import LoginPage from "./pages/LoginPage";
 import NavBar from "./components/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 import { HashRouter, Route, Routes } from "react-router";
 import { UserProvider } from "./context/UserContext";
 import { TweetsProvider } from "./context/TweetsContext";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <HashRouter>
-      <UserProvider>
-        <TweetsProvider>
+      <AuthProvider>
+        <UserProvider>
           <NavBar />
 
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/UserPage" element={<UserPage />} />
+            <Route path="/login" element={<LoginPage />} />
+
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <TweetsProvider>
+                    <HomePage />
+                  </TweetsProvider>
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/UserPage"
+              element={
+                <ProtectedRoute>
+                  <UserPage />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
-        </TweetsProvider>
-      </UserProvider>
+        </UserProvider>
+      </AuthProvider>
     </HashRouter>
   );
 }
