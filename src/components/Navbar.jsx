d483d4763d1a408ffef40ffb3da3a202c93b94d6
DanielChernov99 <chernov.dani@gmail.com>
2026-07-07 17:23:06 +0300

    Refactor NavBar component: integrate authentication logic to conditionally render navigation links based on user authentication status and implement logout functionality.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 import { Paper, Group, Text, Button } from "@mantine/core";
 import classes from "../styles/NavBar.module.css";
+import { useAuth } from "../context/AuthContext";
 
 export default function NavBar() {
+  const navigate = useNavigate();
+  const { isAuthenticated, logout } = useAuth();
+
+  async function handleLogout() {
+    await logout();
+    navigate("/login");
+  }
+
   return (
     <Paper shadow="xs" radius={0} p="md" withBorder>
       <Group justify="space-between">
@@ -11,24 +20,43 @@ export default function NavBar() {
         </Text>
 
         <Group gap="xs">
-          <Button
-            component={NavLink}
-            to="/"
-            end
-            variant="subtle"
-            className={classes.link}
-          >
-            Home
-          </Button>
+          {isAuthenticated && (
+            <>
+              <Button
+                component={NavLink}
+                to="/"
+                end
+                variant="subtle"
+                className={classes.link}
+              >
+                Home
+              </Button>
 
-          <Button
-            component={NavLink}
-            to="/UserPage"
-            variant="subtle"
-            className={classes.link}
-          >
-            Profile
-          </Button>
+              <Button
+                component={NavLink}
+                to="/UserPage"
+                variant="subtle"
+                className={classes.link}
+              >
+                Profile
+              </Button>
+
+              <Button variant="light" color="red" onClick={handleLogout}>
+                Logout
+              </Button>
+            </>
+          )}
+
+          {!isAuthenticated && (
+            <Button
+              component={NavLink}
+              to="/login"
+              variant="subtle"
+              className={classes.link}
+            >
+              Login
+            </Button>
+          )}
         </Group>
       </Group>
     </Paper>
